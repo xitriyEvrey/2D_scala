34bcc798f54ba3d7f1fe66bf753ad2683e9954ba
xitriyEvrey <aqlwiansalen@gmail.com>
2021-01-31 22:59:07 +0300

    Конечная версия

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,9 +14,6 @@ object Main extends App{
 
   val Grid = Array.tabulate[Seq[Point]](200, 200)((_, _) => Seq())
 
-
-  //Grid(0)(0) = Grid(0)(0) :+ Point(1, 1)
-
   BuildGrid()
 
   for (i <- R.indices){
@@ -26,17 +23,15 @@ object Main extends App{
 
 
 
-
-
-  def GetInt(a : Double): Int = (a/10).toInt + (if(a < 0) -1 else 0)
+  def GetIndex(a : Double): Int = (a/10).toInt + (if(a < 0) -1 else 0) + 100
 
   def BuildGrid(): Unit ={
     for (i <- P.indices){
-      Grid(GetInt(P(i).x) + 100)(GetInt(P(i).y) + 100).:+(P(i))
+      Grid(GetIndex(P(i).x))(GetIndex(P(i).y)) :+= P(i)
     }
   }
 
-  def F(points: Seq[Point], x1 : Double, x2 : Double, y1 : Double, y2 : Double): Int ={
+  def CountPoints(points: Seq[Point], x1 : Double, x2 : Double, y1 : Double, y2 : Double): Int ={
     var n : Int = 0
     for (i <- points.indices){
       if (x1 <= points(i).x && points(i).x <= x2 && y1 <= points(i).y && points(i).y <= y2) n += 1
@@ -46,14 +41,14 @@ object Main extends App{
 
   def GetNumberOfPoints(r : Rect): Int ={
     var n : Int = 0
-    val leftIndex = GetInt(r.P1.x)
-    val rightIndex = GetInt(r.P2.x)
-    val lowerIndex = GetInt(r.P1.y)
-    val upperIndex = GetInt(r.P2.y)
+    val leftIndex = GetIndex(r.P1.x)
+    val rightIndex = GetIndex(r.P2.x)
+    val lowerIndex = GetIndex(r.P1.y)
+    val upperIndex = GetIndex(r.P2.y)
     for (i <- leftIndex to rightIndex){
       for (j <- lowerIndex to upperIndex){
         if (i == leftIndex || i == rightIndex || j == lowerIndex || j == upperIndex){
-          n += F(Grid(i)(j), r.P1.x, r.P2.x, r.P1.y, r.P2.y)
+          n += CountPoints(Grid(i)(j), r.P1.x, r.P2.x, r.P1.y, r.P2.y)
         }else{
           n += Grid(i)(j).length
         }
@@ -65,6 +60,5 @@ object Main extends App{
   //////////////
   case class Point(x : Double, y : Double)
 
-  /////////////
   case class Rect(P1 : Point, P2 : Point)
 }
